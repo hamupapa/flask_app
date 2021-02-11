3a8d5ee41e6e65c0b6b8315ab5329b305c68bbfc
hamupapa <k.mine2012@gmail.com>
2021-02-11 13:25:08 +0000

    20210211-2

--- flask_blog/views.py
+++ flask_blog/views.py
@@ -10,3 +10,13 @@ def show_entries():
 def login():
   if request.method == 'POST':
     if request.form['username'] != app.config['USERNAME']:
+      print("ユーザー名が異なります")
+    elif request.form["password"] != app.config["PASSWORD"]:
+      print("パスワードが異なります")
+    else:
+      return redirect("/")
+    return render_template("login.html")
+
+@app.route("/logout")
+def logout():
+  return redirect("/")
