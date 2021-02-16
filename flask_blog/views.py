279b35fb4f58cdc7e94ba635fba7470e2eba9e20
hamupapa <43082939+hamupapa@users.noreply.github.com>
2021-02-16 21:58:20 +0900

    不要なコメント削除

--- flask_blog/views.py
+++ flask_blog/views.py
@@ -6,7 +6,6 @@ from flask_blog import app
 def show_entries():
   if not session.get('logged_in'):
     return redirect(url_for('login'))
-  print("mmm")
   return render_template('entries/index.html')
 
 @app.route('/login', methods=['GET', 'POST'])
