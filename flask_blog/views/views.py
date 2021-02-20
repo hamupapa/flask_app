d051ef19c3ab3a4748b67308eae32b167de0bca2
hamupapa <k.mine2012@gmail.com>
2021-02-20 01:11:03 +0000

    view.py分割

--- flask_blog/views/views.py
+++ flask_blog/views/views.py
@@ -1,12 +1,5 @@
-# flask_blog/views.py
 from flask import request, redirect, url_for, render_template, flash, session
 from flask_blog import app
-
-@app.route('/')
-def show_entries():
-  if not session.get('logged_in'):
-    return redirect(url_for('login'))
-  return render_template('entries/index.html')
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
